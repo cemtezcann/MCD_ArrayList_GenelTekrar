2c15afbf54e7e31f6627212c61673463da4c15c5
cmtzc <tezcancem92@gmail.com>
2021-11-22 16:23:03 +0300

    arraylist anlatıldı.

--- MCD_ArrayList_GenelTekrar/Program.cs
+++ MCD_ArrayList_GenelTekrar/Program.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+namespace MCD_ArrayList_GenelKullanimi
 
-namespace MCD_ArrayList_GenelTekrar
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            #region ArrayList Genel Kullanımı
+
+            //10 elemanlı (string) ArrayList içindeki değerleri Z-A olacak Şekilde sıralayınız.
+
+            ArrayList odevListe = new ArrayList();
+            odevListe.Add("berkecan");
+            odevListe.Add("ögeday");
+            odevListe.Add("ebubekir sıddık");
+            odevListe.Add("murtaza");
+            odevListe.Add("binali");
+            odevListe.Add("yarmagül");
+            odevListe.Add("hamdican");
+            odevListe.Add("rüknettin");
+
+
+            //Tüm değerleri A-z çevir
+            odevListe.Sort();
+            odevListe.Reverse();
+
+            foreach(var item in odevListe)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+
+            #endregion
         }
     }
 }
